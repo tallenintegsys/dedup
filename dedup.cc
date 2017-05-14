81e65a1bea0c686a30db625547ec51bff1f7642f
Tim Allen <tallen@integsys.biz>
2017-05-14 11:01:47 -0400

    I am less than convinced that scandir is the best choice.

--- dedup.cc
+++ dedup.cc
@@ -7,6 +7,7 @@
 typedef uint32_t inode_t;
 typedef uint64_t filesize_t;
 
+// this probably belongs in a .h
 class File {
 	public:
 		inode_t inode;
@@ -21,43 +22,39 @@ class File {
 std::map<inode_t, File *> uk_inode;
 std::multimap<filesize_t, inode_t> cx_size;
 
-
+// this probably belongs in a .c file
 File::File(inode_t i, std::string n, filesize_t s) {
-			inode = i;
-			name = n;
-			size = s;
-			uk_inode.insert(std::pair<inode_t, File *>(inode, this));
-			cx_size.insert(std::pair<filesize_t, inode_t>(size, inode));
-
-		};
+	inode = i;
+	name = n;
+	size = s;
+	uk_inode.insert(std::pair<inode_t, File *>(inode, this));
+	cx_size.insert(std::pair<filesize_t, inode_t>(size, inode));
+};
 
 int main(int argv, char **argc) {
 
-
-	File *f1 = new File(6, "file6", 226);
-	File *f2 = new File(2, "file2", 122);
-	File *f3 = new File(3, "file3", 123);
-	File *f4 = new File(1, "file1", 121);
-	File *f5 = new File(4, "file4", 124);
-	File *f6 = new File(5, "file5", 125);
+	if (argv != 2)
+		return -1;
 
 
 	struct dirent **namelist;
 	int n;
 
-	n = scandir(".", &namelist, NULL, alphasort);
+	n = scandir(argc[1], &namelist, NULL, alphasort);
 	if (n < 0)
 		perror("scandir");
 	else {
 		while (n--) {
 			std::cout << namelist[n]->d_name << std::endl;
-	//		namelist[n]->d_name
-
+			new File(namelist[n]->d_ino, namelist[n]->d_name, namelist[n]->d_off);
 			free(namelist[n]);
 		}
 		free(namelist);
 	}
-
+	for (auto f : uk_inode) {
+		File *ff = f.second;
+		std::cout << ff->inode << "   " << ff->size << "   " << ff->name << std::endl;
+	}
 
 }
 

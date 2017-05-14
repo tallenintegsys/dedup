7e7f90843fdf71ea0fbca7820019a5eb53237125
Tim Allen <tallen@integsys.biz>
2017-05-14 11:58:50 -0400

    This seems more OO

--- dedup.cc
+++ dedup.cc
@@ -7,29 +7,27 @@
 typedef uint32_t inode_t;
 typedef uint64_t filesize_t;
 
-// this probably belongs in a .h
 class File {
+
 	public:
 		inode_t inode;
 		std::string name;
 		filesize_t size;
 		boost::multiprecision::uint128_t md5;
 		boost::multiprecision::uint512_t sha;
+		static std::map<inode_t, File *> uk_inode;
+		static std::multimap<filesize_t, inode_t> cx_size;
 
-		File(inode_t, std::string, filesize_t);
+		File(inode_t i, std::string n, filesize_t s) {
+			inode = i;
+			name = n;
+			size = s;
+			uk_inode.insert(std::pair<inode_t, File *>(inode, this));
+			cx_size.insert(std::pair<filesize_t, inode_t>(size, inode));
+		}
 };
-
-std::map<inode_t, File *> uk_inode;
-std::multimap<filesize_t, inode_t> cx_size;
-
-// this probably belongs in a .c file
-File::File(inode_t i, std::string n, filesize_t s) {
-	inode = i;
-	name = n;
-	size = s;
-	uk_inode.insert(std::pair<inode_t, File *>(inode, this));
-	cx_size.insert(std::pair<filesize_t, inode_t>(size, inode));
-};
+std::map<inode_t, File *> File::uk_inode;
+std::multimap<filesize_t, inode_t> File::cx_size;
 
 int main(int argv, char **argc) {
 
@@ -51,7 +49,7 @@ int main(int argv, char **argc) {
 		}
 		free(namelist);
 	}
-	for (auto f : uk_inode) {
+	for (auto f : File::uk_inode) {
 		File *ff = f.second;
 		std::cout << ff->inode << "   " << ff->size << "   " << ff->name << std::endl;
 	}

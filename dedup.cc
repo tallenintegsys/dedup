e2724d37708d6788c57b4f4a672dce2480dba82d
Tim Allen <tallen@integsys.biz>
2017-05-14 10:16:05 -0400

    warning, does not compile

--- dedup.cc
+++ dedup.cc
@@ -7,38 +7,37 @@
 
 typedef uint32_t inode_t;
 typedef uint64_t filesize_t;
-
-struct file {
-	inode_t inode;
-	std::string name;
-	filesize_t size;
-	boost::multiprecision::uint128_t md5;
-	boost::multiprecision::uint512_t sha;
-};
-
-std::map<inode_t, file *> uk_inode;
+std::map<inode_t, File *> uk_inode;
 std::multimap<filesize_t, inode_t> cx_size;
 
 
-void newFile(inode_t i, std::string n, filesize_t s) {
-	file *f = new file;
-	f->inode = i;
-	f->name = n;
-	f->size = s;
-	uk_inode.insert(std::pair<inode_t, file *>(f->inode, f));
-	cx_size.insert(std::pair<filesize_t, inode_t>(f->size, f->inode));
+class File {
+	public:
+		inode_t inode;
+		std::string name;
+		filesize_t size;
+		boost::multiprecision::uint128_t md5;
+		boost::multiprecision::uint512_t sha;
 
-}
+		File(inode_t i, std::string n, filesize_t s) {
+			inode = i;
+			name = n;
+			size = s;
+			uk_inode.insert(std::pair<inode_t, File *>(inode, this));
+			cx_size.insert(std::pair<filesize_t, inode_t>(size, inode));
+
+		}
+};
 
 int main(int argv, char **argc) {
 
 
-	newFile(6, "file6", 226);
-	newFile(2, "file2", 122);
-	newFile(3, "file3", 123);
-	newFile(1, "file1", 121);
-	newFile(4, "file4", 124);
-	newFile(5, "file5", 125);
+	File *f1 = new File(6, "file6", 226);
+	File *f2 = new File(2, "file2", 122);
+	File *f3 = new File(3, "file3", 123);
+	File *f4 = new File(1, "file1", 121);
+	File *f5 = new File(4, "file4", 124);
+	File *f6 = new File(5, "file5", 125);
 
 
 	struct dirent **namelist;
@@ -50,6 +49,8 @@ int main(int argv, char **argc) {
 	else {
 		while (n--) {
 			std::cout << namelist[n]->d_name << std::endl;
+	//		namelist[n]->d_name
+
 			free(namelist[n]);
 		}
 		free(namelist);

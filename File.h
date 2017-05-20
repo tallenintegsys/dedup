418229c78f6f10d28ed3197c9e909642f2e0c005
Tim Allen <tallen@integsys.biz>
2017-05-20 11:13:57 -0400

    roughing in Doxygen comments

--- File.h
+++ File.h
@@ -9,17 +9,29 @@
 
 typedef off_t fsize_t;
 
+//!  Relevent data about each file in the tree
+/*!
+  This class holds everything related to the files in the the tree.
+  SHA512's are calculated for files whose file sizes don't make it
+  obvious that they are distinct. 
+*/
 class File {
 	public:
 		__ino_t inode;
 		std::string name;
 		std::string relativepath;
 		fsize_t size;
-		unsigned char *sha = NULL;
-		bool hardlink;
+		unsigned char *sha = NULL; /*!< SHA512 for the file */ 
+		bool hardlink; /*!< it's alread a hardlink */ 
 		static std::map<__ino_t, File *> uk_inode;
 		static std::multimap<fsize_t, File *> cx_size;
 
+		 //! populates various variables
+		 /*!
+		   Always populated inode, name, relativepath, and 
+		   size. adds an entry to the inode and size lookup
+		   containers
+		 */
 		File(const std::string &, const std::string &);
 		bool operator== (File &);
 	private:

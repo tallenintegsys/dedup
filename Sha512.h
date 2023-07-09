3a6e701abc4db4738d13151cbb80bd3613eb93f5
tallenintegsys <tallen@integsys.biz>
2023-07-09 17:49:46 -0400

    really, pad left

--- Sha512.h
+++ Sha512.h
@@ -9,11 +9,11 @@ class Sha512 : public std::vector<unsigned char> {
 
 	public:
 	friend auto operator<<(std::ostream &os, const Sha512 &rhs) -> std::ostream & {
-		os << std::hex;
+		os << std::hex << std::setfill('0') << std::right;
 		for (int i = 0; i < 8; i++)
-			os << std::setw(2) << std::setfill('0') << (int)rhs[i];
+			os << std::setw(2) << (int)rhs[i];
 
-		os << std::dec;
+		os << std::dec << std::setfill(' ') << std::left;
 		return os;
 	}
 };
